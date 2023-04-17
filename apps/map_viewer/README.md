b9e7696eaee26e3e5b629c515dfe663ab1a89e86
Leo Conforti <leo@leoconforti.us>
2023-04-17 01:59:28 +0000

    finish map viewer

---
+++ apps/map_viewer/README.md
@@ -0,0 +1,11 @@
+# Map Visualizer
+
+example usage: `bazelisk run //apps/map_viewer '/workspaces/drone_simulation/libs/maps/data/umn.osm' '/workspaces/drone_simulation/apps/map_viewer/'`
+
+produces two images:
+
+1. total.png
+![total graph](./total.png)
+
+2. pruned.png
+![pruned graph](./pruned.png)
--- apps/map_viewer/main.cc
+++ apps/map_viewer/main.cc
@@ -3,29 +3,18 @@
 #include "libs/geometry/bounding_box.h"
 #include "libs/geometry/color.h"
 #include "libs/geometry/image.h"
+#include "libs/geometry/point3f.h"
 #include "libs/maps/graph.h"
+#include "libs/maps/graph_node.h"
 #include "libs/maps/osm_graph.h"
 
 using namespace drone_simulation::geometry;
 using namespace drone_simulation::maps;
 
-int main(int argc, char* argv[]) {
-  if (argc != 3) {
-    std::cerr << "wrong usage, expected \"map_viewer <path/to/map.osm> "
-                 "<path/to/output/image.png>\""
-              << std::endl;
-    return -1;
-  }
-
-  const IGraph* graph = loadOsmGraph(argv[1]);
-  if (graph == nullptr) {
-    std::cout << "Failed to parse graph file" << std::endl;
-    return -2;
-  }
-
+Image drawGraph(const IGraph* graph) {
   BoundingBox bb = graph->getBoundingBox();
   const std::vector<IGraphNode*>& nodes = graph->getNodes();
-  float aspectRatio = (bb.max.y - bb.min.y) / (bb.max.x - bb.min.x);
+  float aspectRatio = (bb.max.x - bb.min.x) / (bb.max.y - bb.min.y);
 
   int resolution = 1024;
   Image output(resolution, resolution * aspectRatio);
@@ -35,19 +24,42 @@ int main(int argc, char* argv[]) {
     Point3f normalizedPoint = bb.normalize(nodes[i]->getPosition());
     const std::vector<IGraphNode*>& neighbors = nodes[i]->getNeighbors();
 
-    int startX = normalizedPoint.x * output.getWidth();
-    int startY = normalizedPoint.y * output.getHeight();
+    int startX = normalizedPoint.y * output.getWidth();
+    int startY = normalizedPoint.x * output.getHeight();
     output.drawPoint(startX, startY, Color(1, 0.5, 0.5, 1));
 
     for (size_t j = 0; j < neighbors.size(); j++) {
       Point3f neighborPos = bb.normalize(neighbors[j]->getPosition());
-      int endX = neighborPos.x * output.getWidth();
-      int endY = neighborPos.y * output.getHeight();
+      int endX = neighborPos.y * output.getWidth();
+      int endY = neighborPos.x * output.getHeight();
       output.drawLine(startX, startY, endX, endY, Color(0.5, 0.5, 1, 1));
     }
   }
 
-  output.saveAs(argv[2]);
+  return output;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc != 3) {
+    std::cerr << "wrong usage, expected \"map_viewer path/to/map.osm "
+                 "path/to/output/folder/\""
+              << std::endl;
+    return -1;
+  }
+
+  IGraph* graph = loadOsmGraph(argv[1], false);
+  if (graph == nullptr) {
+    std::cout << "Failed to parse graph file" << std::endl;
+    return -2;
+  }
+
+  Image output1 = drawGraph(graph);
+  output1.saveAs(std::string(argv[2]) + "total.png");
+
+  graph->prune();
+  Image output2 = drawGraph(graph);
+  output2.saveAs(std::string(argv[2]) + "pruned.png");
+
   delete graph;
   return 0;
 }
