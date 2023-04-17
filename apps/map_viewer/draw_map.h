2ce1cff6578d0c7634c70989c0544d3f40f05ef4
Leo Conforti <leo@leoconforti.us>
2023-04-17 15:26:51 +0000

    refactor

---
+++ apps/map_viewer/draw_map.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "libs/geometry/image.h"
+#include "libs/maps/graph.h"
+
+drone_simulation::geometry::Image drawGraph(
+    const drone_simulation::maps::IGraph* graph);
--- apps/map_viewer/main.cc
+++ apps/map_viewer/main.cc
@@ -1,43 +1,11 @@
 #include <iostream>
 
-#include "libs/geometry/bounding_box.h"
-#include "libs/geometry/color.h"
-#include "libs/geometry/image.h"
-#include "libs/geometry/point3f.h"
+#include "draw_map.h"
 #include "libs/maps/graph.h"
-#include "libs/maps/graph_node.h"
 #include "libs/maps/osm_graph.h"
 
 using namespace drone_simulation::geometry;
 using namespace drone_simulation::maps;
-
-Image drawGraph(const IGraph* graph) {
-  BoundingBox bb = graph->getBoundingBox();
-  const std::vector<IGraphNode*>& nodes = graph->getNodes();
-  float aspectRatio = (bb.max.x - bb.min.x) / (bb.max.y - bb.min.y);
-
-  int resolution = 1024;
-  Image output(resolution, resolution * aspectRatio);
-  output.clear(Color{0, 0, 0, 1});
-
-  for (size_t i = 0; i < nodes.size(); i++) {
-    Point3f normalizedPoint = bb.normalize(nodes[i]->getPosition());
-    const std::vector<IGraphNode*>& neighbors = nodes[i]->getNeighbors();
-
-    int startX = normalizedPoint.y * output.getWidth();
-    int startY = normalizedPoint.x * output.getHeight();
-    output.drawPoint(startX, startY, Color(1, 0.5, 0.5, 1));
-
-    for (size_t j = 0; j < neighbors.size(); j++) {
-      Point3f neighborPos = bb.normalize(neighbors[j]->getPosition());
-      int endX = neighborPos.y * output.getWidth();
-      int endY = neighborPos.x * output.getHeight();
-      output.drawLine(startX, startY, endX, endY, Color(0.5, 0.5, 1, 1));
-    }
-  }
-
-  return output;
-}
 
 int main(int argc, char* argv[]) {
   if (argc != 3) {
