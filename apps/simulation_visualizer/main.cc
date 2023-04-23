63ce3027bae7976c47d847be935d7e477f4ab75b
Leo Conforti <leo@leoconforti.us>
2023-04-23 06:19:31 +0000

    test

--- apps/simulation_visualizer/main.cc
+++ apps/simulation_visualizer/main.cc
@@ -1,1 +1,16 @@
-int main(int argc, char **argv) { return 0; }
+#include "raylib.h"
+
+int main(void) {
+  InitWindow(800, 450, "raylib example - basic window");
+  SetTargetFPS(60);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    DrawText("This is your first window!", 190, 200, 20, LIGHTGRAY);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
