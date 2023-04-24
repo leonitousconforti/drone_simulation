4b870206f39c117fe551bedd4df60234a6d5f22d
Leo Conforti <leo@leoconforti.us>
2023-04-24 17:07:48 +0000

    testing model loading and model animations

--- apps/simulation_visualizer/main.cc
+++ apps/simulation_visualizer/main.cc
@@ -8,6 +8,7 @@
 using bazel::tools::cpp::runfiles::Runfiles;
 
 int main(int argc, char* argv[]) {
+  // Create a runfiles object to load models
   std::string error;
   std::unique_ptr<Runfiles> runfiles(Runfiles::Create(argv[0], &error));
 
@@ -16,11 +17,13 @@ int main(int argc, char* argv[]) {
     return -1;
   }
 
+  // Find the robot model
   std::string robot_model_path = runfiles->Rlocation(
       "drone_simulation/apps/simulation_visualizer/models/robot.glb");
 
+  // Setup the window
   SetTraceLogLevel(LOG_WARNING);
-  InitWindow(800, 450, "raylib example - basic window");
+  InitWindow(800, 450, "Drone Simulation");
   SetTargetFPS(60);
   DisableCursor();
 
@@ -36,7 +39,27 @@ int main(int argc, char* argv[]) {
   Model model = LoadModel(robot_model_path.c_str());
   Vector3 position = {0.0f, 0.0f, 0.0f};
 
+  // Load robot model animations
+  unsigned int animationIndex = 0;
+  unsigned int animationsCount = 0;
+  unsigned int animationCurrentFrame = 0;
+  ModelAnimation* modelAnimations =
+      LoadModelAnimations(robot_model_path.c_str(), &animationsCount);
+
+  // Detects window close button or ESC key
   while (!WindowShouldClose()) {
+    // Select current animation
+    if (IsKeyPressed(KEY_UP))
+      animationIndex = (animationIndex + 1) % animationsCount;
+    else if (IsKeyPressed(KEY_DOWN))
+      animationIndex = (animationIndex + animationsCount - 1) % animationsCount;
+
+    // Update model animation
+    ModelAnimation animation = modelAnimations[animationIndex];
+    animationCurrentFrame = (animationCurrentFrame + 1) % animation.frameCount;
+    UpdateModelAnimation(model, animation, animationCurrentFrame);
+
+    // Draw the scene
     UpdateCamera(&camera, CAMERA_THIRD_PERSON);
     BeginDrawing();
     ClearBackground(RAYWHITE);
@@ -48,6 +71,7 @@ int main(int argc, char* argv[]) {
     EndDrawing();
   }
 
+  // De-initialize
   UnloadModel(model);
   CloseWindow();
   return 0;
